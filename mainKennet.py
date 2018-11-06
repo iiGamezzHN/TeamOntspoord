cc4a10a7057b502a3dc3f657df360c16a4f0cc38
[timok1] <[timokoster1@gmail.com]>
2018-11-06 14:43:09 +0100

    verbetering

--- mainKennet.py
+++ mainKennet.py
@@ -2,7 +2,7 @@ nameList= ["David","Kennet","Timo"]
 
 for name in nameList:
     if name=="Kennet":
-        print(name,",dat is een mooie naam!!")
+        print(name,",dat is een mooie naam!! Er mist alleen een h")
     else:
         print(name,",hmmm")
 
