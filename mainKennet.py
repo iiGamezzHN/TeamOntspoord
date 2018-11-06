f933563c3746c3092167a5a4d80daf82c6be880d
Your Name <you@example.com>
2018-11-06 15:32:44 +0100

    - kleine aanpassingen

--- mainKennet.py
+++ mainKennet.py
@@ -1,14 +1,21 @@
+# Imports
+import os, sys
+
+# de map waarin het project staat
 located_map="TeamOntspoord"
 
-import os, sys
+# pak de parent map
 parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_name+"\\"+located_map+"\\code")
 
-
+# test 
 print(parent_dir_name)
 print(parent_dir_name+"\\"+located_map+"\\code" )
 
-
+# import bestanden vanuit de map code
 import network as nw
+import import_data as imp
 
 nw.import_other_func()
+
+#imp.functionA()
