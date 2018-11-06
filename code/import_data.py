478062d36ddb6c79c321dc901a1f449ca1668b88
Kennet <you@example.com>
2018-11-06 16:04:29 +0100

    - kleine aanpassingen

--- code/import_data.py
+++ code/import_data.py
@@ -2,3 +2,11 @@
 import pandas as pd
 
 #..
+
+def open_stations(location,file):
+    df= pd.read_csv(location+"/"+file,names=["StationA","StationB","Minutes"])
+    print(df)
+
+
+def import_connections():
+    pass
