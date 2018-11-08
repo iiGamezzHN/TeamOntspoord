0eaeb13b20ccac840cd0e95fc3432fe6f161f4ad
[timok1] <[timokoster1@gmail.com]>
2018-11-08 14:25:58 +0100

    random_route zoekt nog naar geschikte kandidaat voor laatste station

--- code/random_route.py
+++ code/random_route.py
@@ -20,11 +20,22 @@ def random_route(network, station, max_length):
         station=random.choice(L_n)
         weight = int(network[station][station_old]['weight'])
 
-        # Make sure you don't go over max length (There still might be another station within max length)
+        # Make sure you don't go over max length, but try to find suitable station
         if tot_length + weight  > max_length:
-            break
+            L_n2 = []
+            for station_2 in L_n:
+                weight_2 = int(network[station_2][station_old]['weight'])
+                if weight_2 + tot_length < max_length:
+                    L_n2.append(station_2)
+
+            if len(L_n2) == 0:
+                break
+            else:
+                station=random.choice(L_n2)
+                weight = int(network[station][station_old]['weight'])
 
         tot_length += weight
         L_route.append(station)
+
     print(L_route)
     print(tot_length)
