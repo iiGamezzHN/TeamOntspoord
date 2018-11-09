cc62d29689c1b16a30500f2c2a38d99813127760
Kennet <you@example.com>
2018-11-09 14:58:51 +0100

    DEFINITIEF - import.py af incl beschrijvingen bij functies

--- code/import_data.py
+++ code/import_data.py
@@ -2,11 +2,12 @@
 import pandas as pd
 
 def open_stations(location,file):
-    """ Deze functie heeft de locatie en de naam van de databestand als input
-    en geeft als output ...
+    """ Deze functie heeft de locatie (folder) en de naam van de file als input
+    en geeft als output een dict van de stations (zonder neighbours!)
+    example > {'stationA': {'Latitude': 52.63777924, 'Longitude': 4.739722252, 'Critical': 'Kritiek'}, ...}
+
     """
     df= pd.read_csv(location+"/"+file,names=["Station","Latitude","Longitude","Critical"])
-
     stations= df['Station']
     stations_critical= df[df.Critical=='Kritiek']["Station"].tolist()
     stations_ncritical= df[df.Critical!='Kritiek']["Station"].tolist()
@@ -16,31 +17,37 @@ def open_stations(location,file):
     station_dict= df.set_index('Station').to_dict('index')
     return station_dict
 
-    # for i in range(len(df)):
-    #     station= df["Station"][i]
-    #     station_loc_dict[station]
-    #     print(df['Station'][i])
-
 
 
 def open_connections(location,file):
+    """ Deze functie heeft de locatie (folder) en de naam van de file als input
+    en geeft als output de connection_list
+    example > [['Den Haag Centraal', 18], ['Alphen a/d Rijn', 19], ['Rotterdam Alexander', 10], ...]
+    """
     df= pd.read_csv(location+"/"+file,names=["StationA","StationB","Minutes"])
     connection_list=[]
-
     for i in range(len(df)):
         connection_list.append([df['StationA'][i],df['StationB'][i],df['Minutes'][i]])
-
-    print(connection_list)
     return connection_list
 
-def add_connections_dict(dict,list):
-    neighboursDict={}
-    for station in dict:
-        for y in list:
-			if station==y[0]:
-				station_neighbours.append([y[1],y[2]])
-			elif station==y[1]:
-				station_neighbours.append([y[0],y[2]])
-		neighboursDict[station]=station_neighbours
 
-        #nog mee bezig
+
+def add_connections_dict(dict,list):
+    """ Deze functie heeft de locatie (folder) en de naam van de databestand als input
+    en geeft als output een dict van de stations (met neighbours!)
+    example > {'stationA': {'Latitude': 52.63777924, 'Longitude': 4.739722252, 'Critical': 'Kritiek', 'Neighbours':[[stationC,13],[stationD,20],[stationE,24]]}, ...}
+    """
+    neighbours_dict={}
+    for station in dict.keys():
+        station_neighbours=[]
+        for y in list:
+            if station==y[0]:
+                station_neighbours.append([y[1],y[2]])
+            elif station==y[1]:
+                station_neighbours.append([y[0],y[2]])
+        neighbours_dict['Neighbours']=station_neighbours
+
+        temp_dict= dict[station]
+        temp_dict['Neighbours']= neighbours_dict['Neighbours']
+        dict[station]=temp_dict
+    return dict
