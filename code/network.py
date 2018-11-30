768d63c3dc10790edd7109cd1a61c9175e4d073a
Kennet <you@example.com>
2018-11-30 12:46:27 +0100

    Grote update - Visualisatie opties

--- code/network.py
+++ code/network.py
@@ -42,18 +42,91 @@ class Network_Graph():
         c_list= [obj.name for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='Kritiek']
         nc_list= [obj.name for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance!='Kritiek']
 
-        nx.draw(self.graph, node_loc_dict,font_size=8,node_size=1,edge_width=0.1,width=0.1)
-        nx.draw_networkx_nodes(self.graph,node_loc_dict,node_size=30,nodelist=c_list,node_color='lightsalmon')
-        nx.draw_networkx_nodes(self.graph,node_loc_dict,node_size=20,nodelist=nc_list,node_color='lightgrey')
-
-        nx.draw_networkx_labels(self.graph,node_loc_dict,labels,font_size=8,font_weight='normal')
-        edge_labels = nx.get_edge_attributes(self.graph,'weight')
-        nx.draw_networkx_edges(self.graph, node_loc_dict, edge_labels=edge_labels, width=0.1, edge_color='k', style='solid')
-        nx.draw_networkx_edge_labels(self.graph, node_loc_dict,edge_labels=edge_labels,font_size=6)
-
-    def plot_graph(self):
-        plt.show(self.graph)
 
     def information(self):
         """Returns the amount of nodes and the nodes themselves """
         return '{} {} {} {} {}'.format('There are',len(self.graph.nodes()),'nodes','\nNodes:',self.graph.nodes())
+
+    def draw_choice(self,option,egdes_option):
+        """ This functions will create a visualisation of the graph. The input is the option to
+        chose which stations to show.
+        standard= a graph with all the stations
+        critical= a graph with only the critical stations
+        track= a graph with only the stations from the track and critical stations
+        tracks= shows a graph, all the critical edges are blue, the remaining critical edges are red
+        The second input is the edges_option, if its true all the edge labels will show
+        """
+        graphdict={}
+        locations= [(obj.name,obj.location) for obj in gc.get_objects() if isinstance(obj, self.station_class)]
+        graphdict= {x[0]:x[1] for x in locations}
+
+        c_list= [obj.name for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='critical']
+        nc_list= [obj.name for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='not critical']
+
+        nx.draw(self.graph, graphdict,font_size=8,node_size=1,edge_width=0.1,width=0.1)
+        nx.draw_networkx_nodes(self.graph,graphdict,node_size=30,nodelist=c_list,node_color='lightsalmon')
+        nx.draw_networkx_nodes(self.graph,graphdict,node_size=20,nodelist=nc_list,node_color='lightgrey')
+
+        if option=='standard':
+        labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class)}
+        elif option=='critical':
+        labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='critical'}
+        elif option[0]=='track':
+        tracks= option[1]
+        for x in tracks:
+            track = x[0]
+            time = x[1]
+            templist=[]
+            edge_labels={}
+            for y in track:
+                templist.append(y[0][0])
+                templist.append(y[0][1])
+                edge_labels[(y[0][0],y[0][1])]=y[1]
+            tracklist= set(templist)
+            t_labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.name in tracklist}
+            r_labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='critical' and obj.name not in t_labels}
+
+            nx.draw(self.graph, graphdict,font_size=8,node_size=1,edge_width=0.1,width=0.1)
+            nx.draw_networkx_nodes(self.graph,graphdict,node_size=30,nodelist=c_list,node_color='lightsalmon')
+            nx.draw_networkx_nodes(self.graph,graphdict,node_size=20,nodelist=nc_list,node_color='lightgrey')
+
+            nx.draw_networkx_edges(self.graph, graphdict,edgelist=edge_labels,width=0.5, edge_color='blue', style='solid',with_label=True)
+            nx.draw_networkx_edge_labels(self.graph, graphdict,edge_labels=edge_labels,font_size=6)
+            nx.draw_networkx_labels(self.graph,graphdict,t_labels,font_size=8,font_weight='bold')
+            nx.draw_networkx_labels(self.graph,graphdict,r_labels,font_size=8,font_weight='normal')
+            plt.show(self.graph)
+
+        elif option[0]=='all tracks':
+        tracks= option[1]
+        templist=[]
+        edge_labels={}
+        for x in tracks:
+        	track= x[0]
+        	time= x[1]
+        	for y in track:
+        		templist.append(y[0][0])
+        		templist.append(y[0][1])
+        		edge_labels[(y[0][0],y[0][1])]=y[1]
+        tracklist= set(templist)
+        t_labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.name in tracklist}
+        r_labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='critical' and obj.name not in t_labels}
+        b_labels= {obj.name:obj.label for obj in gc.get_objects() if isinstance(obj, self.station_class) if obj.importance=='critical'}
+
+
+        nx.draw(self.graph, graphdict,font_size=8,node_size=1,edge_width=0.1,width=0.1)
+        nx.draw_networkx_nodes(self.graph,graphdict,node_size=30,nodelist=c_list,node_color='lightsalmon')
+        nx.draw_networkx_nodes(self.graph,graphdict,node_size=20,nodelist=nc_list,node_color='lightgrey')
+
+        #draw every critical edge red (if the track covers this edge it will become blue)
+        critical_labels={(x[0][0],x[0][1]):x[1] for x in option[2]}
+        red_labels={x:critical_labels[x] for x in critical_labels if x not in edge_labels and (x[1],x[0]) not in edge_labels}
+        print(red_labels)
+
+        nx.draw_networkx_edges(self.graph, graphdict,edgelist=red_labels,width=1, edge_color='red', style='solid',with_label=True)
+
+        nx.draw_networkx_edges(self.graph, graphdict,edgelist=edge_labels,width=0.5, edge_color='blue', style='solid',with_label=True)
+        #nx.draw_networkx_edge_labels(self.graph, graphdict,edge_labels=edge_labels,font_size=6)
+        nx.draw_networkx_labels(self.graph,graphdict,b_labels,font_size=8,font_weight='bold')
+        #nx.draw_networkx_labels(self.graph,graphdict,t_labels,font_size=8,font_weight='bold')
+        #nx.draw_networkx_labels(self.graph,graphdict,r_labels,font_size=8,font_weight='normal')
+        plt.show(self.graph)
