2a736c248205d97e82ca4c0bab293705cb8ae193
[timok1] <[timokoster1@gmail.com]>
2018-12-17 18:59:51 +0100

    Fixed main hopefully

--- main.py
+++ main.py
@@ -27,12 +27,11 @@ import random_route as rr
 import transform_tracklist as tt
 
 # import files using the functions from import_data.py
-import_name = "Nationaal"
 
-if import_name == "Holland":
+if sys.argv[1] == "Holland":
     import_dict = imp.open_stations('data', 'StationsHolland.csv')
     import_list = imp.open_connections('data', 'ConnectiesHolland.csv')
-if import_name == "Nationaal":
+if sys.argv[1] == "Nationaal":
     import_dict = imp.open_stations('data', 'StationsNationaal.csv')
     import_list = imp.open_connections('data', 'ConnectiesNationaal.csv')
 
@@ -60,8 +59,9 @@ print(stations['Amsterdam Centraal'].information())
 
 
 # Create the network
-G = nw.Network_Graph(st.Station)
-print(G.information())
+L = nw.Network_Graph(st.Station)
+print(L.information())
+
 
 apct = sc.unique(station_dict)[0]
 uct = sc.unique(station_dict)[1]
@@ -89,12 +89,12 @@ def depth_first_look_ahead(region, all, n_best):
     tot_crit_tracks = len(L_crit_tracks)
     parameters = pc.Parameters(G, max_length, tot_crit_tracks, L_station)
     solution_set = la(parameters, n_best, L_crit_tracks)
-    solution_list = []
+    solution_string = ''
 
     # Print solutions, create graph
     for object in solution_set[0]:
         print(object.L_route)
-        solution_list.append(object.L_route)
+        solution_string += (str(object.L_route) + '\n')
     print(solution_set[1])
     plt.plot(solution_set[2], 'o-')
     plt.xlabel('Amount of routes found (0 is the route without look-ahead)')
@@ -102,12 +102,12 @@ def depth_first_look_ahead(region, all, n_best):
     plt.title('Evolution of K-score with depth first with look ahead')
     if all:
         with open("resultaten/depth_look_"+region+"_all.txt", "a") as myfile:
-            myfile.write(solution_list)
+            myfile.write(solution_string)
             myfile.write(solution_set[1])
         plt.savefig("resultaten/depth_look" + region + 'All')
     else:
         with open("resultaten/depth_look_"+region+"_notAll.txt", "a") as myfile:
-            myfile.write(str(solution_list))
+            myfile.write(str(solution_string))
             myfile.write(str(solution_set[1]))
         plt.savefig("resultaten/depth_look" + region + 'notAll')
     plt.show()
@@ -135,11 +135,12 @@ def depth_first_random(region, all, n_best, n_iterations):
     tot_crit_tracks = len(L_crit_tracks)
     parameters = pc.Parameters(G, max_length, tot_crit_tracks, L_station)
     solution_set = dr(parameters, n_best, L_crit_tracks, n_iterations)
+    solution_string = ''
 
     # Print solutions, create graph
     for object in solution_set[0]:
         print(object.L_route)
-        print(object.tot_weight)
+        solution_string += (str(object.L_route) + '\n')
     print(solution_set[1])
     plt.plot(solution_set[2], '-')
     plt.xlabel('Iterations')
@@ -147,31 +148,37 @@ def depth_first_random(region, all, n_best, n_iterations):
     plt.title('Evolution of K-score with depth first with random choice')
     if all:
         plt.savefig("resultaten/depth_random" + region + 'All')
+        with open("resultaten/depth_look_"+region+"_all.txt", "a") as myfile:
+            myfile.write(str(solution_string))
+            myfile.write(str(solution_set[1]))
     else:
         plt.savefig("resultaten/depth_random" + region + 'notAll')
+        with open("resultaten/depth_look_"+region+"_all.txt", "a") as myfile:
+            myfile.write(str(solution_string))
+            myfile.write(str(solution_set[1]))
     plt.show()
 
 
 if __name__ == "__main__":
     # Running depth first with random selection
-    if sys.argv[1] == 'depth_first_random':
-        if len(sys.argv) == 2:
-            depth_first_random('Holland', False, 30, 1000)
+    if sys.argv[2] == 'depth_first_random':
+        if len(sys.argv) == 3:
+            depth_first_random(sys.argv[1], False, 30, 50)
         else:
             try:
-                depth_first_random(sys.argv[2], sys.argv[3], int(sys.argv[4]),
+                depth_first_random(sys.argv[1], sys.argv[3], int(sys.argv[4]),
                                    int(sys.argv[5]))
             except:
                 print("Invalid Input: usage: python main.py depth_first_random"
                       "region all_critical n_best iterations")
 
     # Running depth first with look ahead
-    if sys.argv[1] == 'depth_first_look_ahead':
-        if len(sys.argv) == 2:
-            depth_first_look_ahead('Holland', False, 50)
+    if sys.argv[2] == 'depth_first_look_ahead':
+        if len(sys.argv) == 3:
+            depth_first_look_ahead(sys.argv[1], False, 20)
         else:
             try:
-                depth_first_look_ahead(sys.argv[2], sys.argv[3],
+                depth_first_look_ahead(sys.argv[1], sys.argv[3],
                                        int(sys.argv[4]))
             except:
                 print("Invalid Input: usage: python main.py"
@@ -179,9 +186,9 @@ if __name__ == "__main__":
 
     # Draw the variable 'tracks', the edges are green if they are vistited once
     # and the are red when they are vistied multiple times
-    if sys.argv[1] == 'draw_routes':
+    if sys.argv[2] == 'draw_routes':
         tracks = [['Den Helder', 'Alkmaar', 'Castricum', 'Zaandam', 'Amsterdam Sloterdijk', 'Amsterdam Centraal', 'Amsterdam Sloterdijk', 'Haarlem', 'Heemstede-Aerdenhout', 'Leiden Centraal', 'Den Haag HS', 'Delft', 'Den Haag Centraal', 'Leiden Centraal', 'Schiphol Airport', 'Amsterdam Zuid'],['Maastricht', 'Sittard', 'Heerlen', 'Sittard', 'Roermond', 'Weert', 'Eindhoven', 's-Hertogenbosch', 'Tilburg', 'Breda', 'Dordrecht', 'Rotterdam Centraal', 'Schiedam Centrum'],['Enschede', 'Hengelo', 'Almelo', 'Zwolle', 'Deventer', 'Zutphen', 'Dieren', 'Arnhem Centraal', 'Ede-Wageningen', 'Utrecht Centraal', 'Gouda'],['Hoorn', 'Alkmaar', 'Castricum', 'Beverwijk', 'Haarlem', 'Amsterdam Sloterdijk', 'Amsterdam Centraal', 'Amsterdam Amstel', 'Utrecht Centraal', 'Alphen a/d Rijn', 'Leiden Centraal', 'Den Haag Laan v NOI'],['Steenwijk', 'Zwolle', 'Amersfoort', 'Utrecht Centraal', 'Hilversum', 'Utrecht Centraal', 's-Hertogenbosch', 'Oss', 'Nijmegen', 'Arnhem Centraal'],['Heerenveen', 'Leeuwarden', 'Groningen', 'Assen', 'Zwolle'],['Helmond', 'Eindhoven', 'Tilburg', 'Breda', 'Etten-Leur', 'Roosendaal', 'Dordrecht', 'Rotterdam Blaak', 'Rotterdam Alexander', 'Rotterdam Centraal', 'Schiedam Centrum', 'Delft', 'Den Haag HS', 'Gouda', 'Den Haag Centraal'],['Schiphol Airport', 'Utrecht Centraal', 'Amsterdam Centraal', 'Almere Centrum']]
 
-        print(sc.score(G.graph,tracks,apct,uct)[0])
+        print(sc.score(L.graph,tracks,apct,uct)[0])
         # L.draw_choice(['track',tt.transform(L.graph,endtest)[0],uct],egdes_option=False)
-        G.draw_choice(['all tracks',tt.transform(G.graph,tracks)[0],uct],egdes_option=False)
+        L.draw_choice(['all tracks',tt.transform(L.graph,tracks)[0],uct],egdes_option=False)
